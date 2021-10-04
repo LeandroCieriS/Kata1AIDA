bcbe728abac141b4055fd174f6e568c42b03fc8a
LeandroCieriS <leandro.cieri.s@gmail.com>
2021-10-04 17:01:03 +0100

    green return_addition_when_input_is_any_amount_of_numbers

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,4 +1,7 @@
-﻿namespace StringCalculator
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculator
 {
     public static class StringCalculator 
     {
@@ -10,17 +13,22 @@
             if (input.Contains(SEPARATOR))
             {
                 var transformedInput = Transform(input);
-                return transformedInput[0] + transformedInput[1];
+                return transformedInput.Sum();
             }
             
             return int.Parse(input);
         }
 
 
-        private static int[] Transform(string input)
+        private static IEnumerable<int> Transform(string input)
         {
             var splittedInput = input.Split(SEPARATOR);
-            return new int[] { int.Parse(splittedInput[0]), int.Parse(splittedInput[1]) };
+            var parsedInput = new List<int>();
+            
+            foreach (var number in splittedInput)
+                parsedInput.Add(int.Parse(number));
+
+            return parsedInput.ToArray();
         }
     }
 }

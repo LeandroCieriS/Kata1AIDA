01c239c785ef2e49a6ea86e49e2ee12b01b9cbec
LeandroCieriS <leandro.cieri.s@gmail.com>
2021-10-04 16:39:55 +0100

    refactor extract method transform input

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,20 +1,27 @@
-﻿using System;
-
-namespace StringCalculator {
+﻿namespace StringCalculator
+{
     public static class StringCalculator 
     {
         private const string SEPARATOR = ",";
 
         public static int Add(string input){
-            if (string.IsNullOrEmpty(input)) return 0;
+            if (string.IsNullOrEmpty(input)) 
+                return 0;
             if (input.Contains(SEPARATOR))
             {
-                var splittedInput = input.Split(",");
-                return int.Parse(splittedInput[0]) + int.Parse(splittedInput[1]);
+                var transformedInput = Transform(input);
+                return transformedInput[0] + transformedInput[1];
             }
+            
             return int.Parse(input);
         }
 
+
+        private static int[] Transform(string input)
+        {
+            var splittedInput = input.Split(SEPARATOR);
+            return new int[] { int.Parse(splittedInput[0]), int.Parse(splittedInput[1]) };
+        }
     }
 }
 

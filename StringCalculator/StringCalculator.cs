6281e95006180a3b2981430a2dc1f40c304895a1
LeandroCieriS <leandro.cieri.s@gmail.com>
2021-10-07 11:01:53 +0100

    green return_addition_when_delimiter_is_provided_by_input

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -5,15 +5,16 @@ namespace StringCalculator
 {
     public static class StringCalculator 
     {
-        private const string SEPARATOR_COMMA = ",";
-        private const string SEPARATOR_NEW_LINE = "\n";
+        private const char SEPARATOR_COMMA = ',';
+        private const char SEPARATOR_NEW_LINE = '\n';
+        private const string DELIMITER_SELECTOR = "//";
 
         public static int Add(string input)
         {
             if (string.IsNullOrEmpty(input)) 
                 return 0;
-            if (!input.Contains(SEPARATOR_COMMA) && !input.Contains(SEPARATOR_NEW_LINE)) 
-                return int.Parse(input);
+            if (input.Contains(DELIMITER_SELECTOR))
+                input = ReplaceDelimiter(input);
             return Transform(input).Sum();
 
         }
@@ -22,6 +23,13 @@ namespace StringCalculator
         {
             input = input.Replace(SEPARATOR_NEW_LINE, SEPARATOR_COMMA);
             return input.Split(SEPARATOR_COMMA).Select(int.Parse);
+        }
+
+        private static string ReplaceDelimiter(string input)
+        {
+            var newDelimiter = input[2];
+            input = input.Substring(4);
+            return input.Replace(newDelimiter, SEPARATOR_COMMA);
         }
     }
 }
